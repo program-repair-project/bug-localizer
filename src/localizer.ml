050cf2c0931b88c498a9b4d8ac965cb136464b58
KAIST-JongchanPark <whdals1060@kaist.ac.kr>
2021-07-29 10:59:57 +0900

    [Localizer] add tarantula

--- src/localizer.ml
+++ src/localizer.ml
@@ -37,7 +37,7 @@ let dummy_localizer work_dir bug_desc =
         elem.LineCoverage.coverage locs)
     [] coverage
 
-let prophet_localizer work_dir bug_desc =
+let spec_localizer work_dir bug_desc =
   let coverage = LineCoverage.run work_dir bug_desc in
   Logging.log "Coverage: %a" LineCoverage.pp coverage;
   copy_src ();
@@ -69,24 +69,62 @@ let prophet_localizer work_dir bug_desc =
           Hashtbl.replace table l (s1 +. new_s1, s2 +. new_s2, s3 +. new_s3)
       | _ -> Hashtbl.add table l (s1, s2, s3))
     locations;
-  let new_locations =
-    List.map
-      (fun (l, (s1, s2, s3)) -> (l, s1, s2, s3))
-      (List.of_seq (Hashtbl.to_seq table))
-  in
+  List.map
+    (fun (l, (s1, s2, s3)) -> (l, s1, s2, s3))
+    (List.of_seq (Hashtbl.to_seq table))
+
+let prophet_localizer work_dir bug_desc =
+  let locations = spec_localizer work_dir bug_desc in
   List.stable_sort
     (fun (_, s11, s12, s13) (_, s21, s22, s23) ->
       if s21 -. s11 <> 0. then int_of_float (s21 -. s11)
       else if s12 -. s22 <> 0. then int_of_float (s12 -. s22)
       else int_of_float (s23 -. s13))
-    new_locations
+    locations
+
+let tarantula_localizer work_dir bug_desc =
+  let locations = spec_localizer work_dir bug_desc in
+  let test_cases = bug_desc.BugDesc.test_cases in
+  let pos_num =
+    List.fold_left
+      (fun acc t ->
+        let regexp_pos = Str.regexp "p.*" in
+        if Str.string_match regexp_pos t 0 then acc + 1 else acc)
+      0 test_cases
+  in
+  let neg_num =
+    List.fold_left
+      (fun acc t ->
+        let regexp_neg = Str.regexp "n.*" in
+        if Str.string_match regexp_neg t 0 then acc + 1 else acc)
+      0 test_cases
+  in
+  let taran_loc =
+    List.map
+      (fun (l, s1, s2, _) ->
+        let nep = s2 in
+        let nnp = float_of_int pos_num -. s2 in
+        let nef = s1 in
+        let nnf = float_of_int neg_num -. s1 in
+        let numer = nef /. (nef +. nnf) in
+        let denom1 = nef /. (nef +. nnf) in
+        let denom2 = nep /. (nep +. nnp) in
+        let score = numer /. (denom1 +. denom2) in
+        print_endline (string_of_float score);
+        (l, s1, s2, score))
+      locations
+  in
+  List.stable_sort
+    (fun (_, _, _, s13) (_, _, _, s23) ->
+      if s23 > s13 then 1 else if s23 = s13 then 0 else -1)
+    taran_loc
 
 let run work_dir =
   Logging.log "Start localization";
   let bug_desc = BugDesc.read work_dir in
   Logging.log "Bug desc: %a" BugDesc.pp bug_desc;
   match !Cmdline.engine with
-  | Cmdline.Tarantula -> failwith "Not implemented"
+  | Cmdline.Tarantula -> tarantula_localizer work_dir bug_desc
   | Cmdline.Prophet -> prophet_localizer work_dir bug_desc
   | Cmdline.Dummy -> dummy_localizer work_dir bug_desc
 
